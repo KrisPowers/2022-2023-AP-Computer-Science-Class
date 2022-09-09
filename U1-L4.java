a62fd580821d3cb512068c785b15d80ba9e9a716
Kris Powers <85710701+KrisPowers@users.noreply.github.com>
2022-09-09 18:34:13 -0400

    Programmed on: 09/09/2022

---
+++ U1-L4.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import static java.lang.System.*;
+import java.util.Scanner;
+
+class main{
+  
+  public static void main (String str[]) throws IOException {
+    
+    Scanner scan = new Scanner(System.in);
+    
+    int num1 = 18;
+    int num2 = 3;
+    System.out.println(num1 + num2);
+  
+  }
+}
